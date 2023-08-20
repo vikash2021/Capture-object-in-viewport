63942513b4e07bd5aaf2712cfe35973e986c597e
Vikash Kumar <kumarv4624@gmail.com>
2023-08-20 22:13:52 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+
+
+document.getElementById("app").innerHTML = `
+<h1>Men Shirts</h1>
+<div>
+<div class="blocks">Allen Solly</div>
+<div class="blocks">Peter England</div>
+<div class="blocks">Turtle</div>
+<div class="blocks">Tribe</div>
+<div class="blocks">Ninja</div>
+<div class="blocks">Blackberry</div>
+<div class="blocks">Louis Phillipe</div>
+<div class="blocks">Louis Vouitton</div>
+<div class="blocks">Parks</div>
+<div class="blocks">Sparko</div>
+<div class="blocks">Arrow</div>
+<div class="blocks">Van Huesen</div>
+<div class="blocks">Turtle</div>
+<div class="blocks">Mufti</div>
+<div class="blocks">Monte Carlo</div>
+<div class="blocks">Allen Cooper</div>
+<div class="blocks">Jack & Jones</div>
+<div class="blocks">Lacoste</div>
+<div class="blocks">Mettle</div>
+
+</div>
+`;
